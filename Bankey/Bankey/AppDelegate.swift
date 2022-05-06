9010a3b559b351a454d2e9b9ee931b8ed6a4ecdb
luism3861 <Eduardo_Medina@fragua.com.mx>
2022-05-05 19:12:29 -0500

    delete space

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame:  UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .green
-        window?.rootViewController =  ViewController()
+        window?.rootViewController = ViewController()
         return true
     }
 }
