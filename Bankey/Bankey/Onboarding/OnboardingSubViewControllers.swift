2c313c12c33e8f6081daa52907a2d825dd040fbf
luism3861 <Eduardo_Medina@fragua.com.mx>
2022-05-19 18:50:39 -0500

    add onboarding view controller

---
+++ Bankey/Bankey/Onboarding/OnboardingSubViewControllers.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingSubControllers.swift
+//  Bankey
+//
+//  Created by Luis Eduardo Madina Huerta on 19/05/22.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class ViewController3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
