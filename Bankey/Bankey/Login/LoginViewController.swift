db68fdebd4e2b4aa0c09955ff69ad00d16fe6545
luism3861 <Eduardo_Medina@fragua.com.mx>
2022-06-17 12:45:31 -0500

    fix

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -72,7 +72,7 @@ extension LoginViewController{
         subtitleLabel.textAlignment = .center
         subtitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
         subtitleLabel.adjustsFontSizeToFitWidth = true
-        subtitleLabel.text = "Your premium source for all things banking!"
+        subtitleLabel.text = "Your premium source for all things ps we!"
         
         loginView.translatesAutoresizingMaskIntoConstraints = false
 
--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -5,4 +5,26 @@
 //  Created by Luis Eduardo Madina Huerta on 10/06/22.
 //
 
-import Foundation
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    let stackView = UIStackView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+
+extension OnboardingViewController{
+    func style(){
+        print("hola")
+    }
+    
+    func layout(){
+        print("we")
+    }
+}
